e711e20808a9d9ea0d6ac8bb6094dd4e40ed1968
97saundersj <jacques_s@hotmail.co.uk>
2021-11-22 21:58:23 +0000

    Added Inital round system

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,72 +7,118 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public TextMesh Text;
+    public GestureListener gestureListener;
+    public TextMesh gameText;
     public RiggedHand hand;
-    // Next update in second
-    private int nextUpdate = 1;
+
+    private string selectedGesture;
+
+    // the start time of the round in seconds
+    private float roundStartTime = -1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        selectedGesture = gestureListener.selectedGesture;
+        Debug.Log("current Gesture: " + selectedGesture);
         // If the next update is reached
-        //if (Time.time >= nextUpdate)
+        if (roundStartTime <= -1)
         {
-            // Change the next update (current second+1)
-            nextUpdate = Mathf.FloorToInt(Time.time) + 1;
-
-            if(hand.IsTracked)
+            if (selectedGesture == "thumbsUp")
             {
-                var rockStrength = GetRockStrength(hand.GetLeapHand());
-                var paperStrength = GetPaperStrength(hand.GetLeapHand());
-                var sissorsStrength = GetSissorsStrength(hand.GetLeapHand());
-                var fuStrength = GetFUStrength(hand.GetLeapHand());
-
-                var estimatedGesture = Math.Max(rockStrength, Math.Max(paperStrength, sissorsStrength));
-                
-                if(rockStrength > paperStrength && rockStrength > sissorsStrength && rockStrength > fuStrength)
-                {
-                    Text.text = "ROCK!";
-                    Debug.Log("ROCK!");
-                }
-                else if (paperStrength > rockStrength && paperStrength > sissorsStrength && paperStrength > fuStrength)
-                {
-                    Text.text = "PAPER!";
-                    Debug.Log("PAPER!");
-                }
-                else if (sissorsStrength > rockStrength && sissorsStrength > paperStrength && sissorsStrength > fuStrength)
-                {
-                    Text.text = "SISSORS!";
-                    Debug.Log("SISSORS!");
-                }
-
-                else if (fuStrength > rockStrength && fuStrength > paperStrength && fuStrength > sissorsStrength)
-                {
-                    Text.text = "Wow, that was rude!";
-                    Debug.Log("Rude!");
-                }
+                gameText.text = "Round Start, make your choice.";
+                roundStartTime = Mathf.FloorToInt(Time.time);
             }
-            else
+        }
+        else
+        {
+            var roundTime =  Mathf.FloorToInt(Time.time) - roundStartTime;
+            Debug.Log("roundTime: " + roundTime);
+            if (roundTime <= 1)
             {
-                Text.text = "Make your choice";
-                //Debug.Log("Untracked!");
+                gameText.text = "3...";
             }
-            /*
-            switch(estimatedGesture)
+            else if (roundTime <= 2)
             {
-                case rockStrength > Math.Max(paperStrength, sissorsStrength):
-                    Debug.Log("Fist Strength: " + );
-                    break;
+                gameText.text = "2...";
             }
-            */
+            else if (roundTime <= 3)
+            {
+                gameText.text = "1...";
+            }
+            else if (roundTime <= 4)
+            {
+                gameText.text = "Go!";
+            }
+            else if (roundTime <= 5)
+            {
+                //Make the Randomly pick a gesture for the AI
+                string[] gestures = { "rock", "paper", "sissors" };
+                int randomIndex = UnityEngine.Random.Range(0, 3);
+                var gestureAI = gestures[randomIndex];
 
-            //Debug.Log("Fist Strength: " + );
+                var gesturePlayer = selectedGesture;
+
+                if(gesturePlayer == "rock")
+                {
+                    gameText.text = "You picked: Rock\n";
+                    if (gestureAI == "rock")
+                    {
+                        gameText.text += "The AI picked: Rock\nDRAW!";
+                    }
+                    else if(gestureAI == "paper")
+                    {
+                        gameText.text += "The AI picked: Paper\nYOU Lose!";
+                    }
+                    else if (gestureAI == "sissors")
+                    {
+                        gameText.text += "The AI picked: Sissors\nYOU WIN!";
+                    }
+                }
+                else if (gesturePlayer == "paper")
+                {
+                    gameText.text = "You picked: Paper\n";
+                    if (gestureAI == "rock")
+                    {
+                        gameText.text += "The AI picked: Rock\nYOU WIN!";
+                    }
+                    else if (gestureAI == "paper")
+                    {
+                        gameText.text += "The AI picked: Paper\nDRAW!";
+                    }
+                    else if (gestureAI == "sissors")
+                    {
+                        gameText.text += "The AI picked: Sissors\nYOU LOSE!";
+                    }
+                }
+                else if (gesturePlayer == "sissors")
+                {
+                    gameText.text = "You picked: Sissors\n";
+                    if (gestureAI == "rock")
+                    {
+                        gameText.text += "The AI picked: Rock\nLoose!";
+                    }
+                    else if (gestureAI == "paper")
+                    {
+                        gameText.text += "The AI picked: Paper\nYOU Win!";
+                    }
+                    else if (gestureAI == "sissors")
+                    {
+                        gameText.text += "The AI picked: Sissors\nYOU Draw!";
+                    }
+                }
+
+                gameText.text += "\nThumbs Up to play agin :)";
+                 
+                //gameText.text = "You have selected{selectedGesture}";
+                roundStartTime = -1;
+            }
         }
     }
 
--- Assets/Scripts/GestureListener.cs
+++ Assets/Scripts/GestureListener.cs
@@ -30,17 +30,24 @@ public class GestureListener : MonoBehaviour
             nextUpdate = Mathf.FloorToInt(Time.time) + 1;
 
             if (hand.IsTracked)
-            {
+            {   
                 var gestureDictionary = new Dictionary<string, float>();
-                gestureDictionary.Add("rockStrength", GetRockStrength(hand.GetLeapHand()));
-                gestureDictionary.Add("paperStrength", GetPaperStrength(hand.GetLeapHand()));
-                gestureDictionary.Add("sissorsStrength", GetSissorsStrength(hand.GetLeapHand()));
-                gestureDictionary.Add("fuStrength", GetFUStrength(hand.GetLeapHand()));
+                var leapHand = hand.GetLeapHand();
+
+                gestureDictionary.Add("thumbsUpStrength", GetThumbsUpStrength(leapHand));
+                gestureDictionary.Add("rockStrength", GetRockStrength(leapHand));
+                gestureDictionary.Add("paperStrength", GetPaperStrength(leapHand));
+                gestureDictionary.Add("sissorsStrength", GetSissorsStrength(leapHand));
+                gestureDictionary.Add("fuStrength", GetFUStrength(leapHand));
 
                 var chosenGesture = gestureDictionary.FirstOrDefault(x => x.Value.Equals(gestureDictionary.Values.Max()));
 
                 switch (chosenGesture.Key)
                 {
+                    case "thumbsUpStrength":
+                        selectedGesture = "thumbsUp";
+                        text.text = "Okay, lets play :)";
+                        break;
                     case "rockStrength":
                         selectedGesture = "rock";
                         text.text = "ROCK!";
@@ -61,9 +68,9 @@ public class GestureListener : MonoBehaviour
             }
             else
             {
-                selectedGesture = "untracked";
+                //selectedGesture = "untracked";
                 text.text = "Make your choice";
-                Debug.Log(selectedGesture);
+                //Debug.Log(selectedGesture);
             }
         }
     }
@@ -121,15 +128,15 @@ public class GestureListener : MonoBehaviour
     }
 
     /// <summary>
-    /// Returns a confidence value from 0 to 1 indicating how strongly the Hand is making a fist.
+    /// Returns a confidence value from 0 to 1 indicating how strongly the Hand is making a Thumbs up.
     /// </summary>
-    public static float GetPointingStrength(Hand hand)
+    public static float GetThumbsUpStrength(Hand hand)
     {
-        return (Vector3.Dot(hand.Fingers[1].Direction.ToVector3(), hand.DistalAxis())
+        return (Vector3.Dot(hand.Fingers[1].Direction.ToVector3(), -hand.DistalAxis())
               + Vector3.Dot(hand.Fingers[2].Direction.ToVector3(), -hand.DistalAxis())
               + Vector3.Dot(hand.Fingers[3].Direction.ToVector3(), -hand.DistalAxis())
               + Vector3.Dot(hand.Fingers[4].Direction.ToVector3(), -hand.DistalAxis())
-              + Vector3.Dot(hand.Fingers[0].Direction.ToVector3(), -hand.RadialAxis())
+              + Vector3.Dot(hand.Fingers[0].Direction.ToVector3(), hand.RadialAxis())
               ).Map(-5, 5, 0, 1);
     }
 }
